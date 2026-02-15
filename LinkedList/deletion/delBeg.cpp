82d2145886cfda0b32fd8d17162c921ca11c6ecb
TEAROFJOY5 <179818307+TEAROFJOY5@users.noreply.github.com>
2026-02-14 20:31:57 -0800

    Add delBeg function to delete first node
    Implement deletion of the first node in a singly linked list.

---
+++ LinkedList/deletion/delBeg.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+using namespace std;
+// #include<stdin.h>
+struct node
+    {
+        int data;
+        struct node *next;
+    };
+
+node* CreatLL(){ //This will create Singly Linked List
+    
+    struct node *head, *newNode, *temp;
+    head = 0;
+    int choice = 1;
+    while(choice)
+    {
+        // newNode = 
+        newNode = new struct node;
+        cout<<"Enter data: "<<endl;
+        cin>>newNode->data;
+        newNode->next = nullptr;
+        if (head == 0)
+        {
+            head = temp = newNode;
+        }
+        else
+        {
+            temp->next = newNode; // link to previous node
+            temp = newNode; // move temp to newNode.
+
+        }
+       while (true)
+    {
+        cout << "Enter 1 to continue or 0 to stop: ";
+        cin >> choice;
+
+        if (cin.fail())   // user entered letters like abc
+        {
+            cin.clear();  
+            cin.ignore(1000, '\n');
+            cout << "Invalid input! Please enter 0 or 1 only.\n";
+        }
+        else if (choice == 0 || choice == 1)
+        {
+            break;   // valid input
+        }
+        else
+        {
+            cout << "Invalid input! Please enter only 0 or 1.\n";
+        }
+    }
+ }
+ 
+ return head;
+}
+
+// int count(struct node* head)
+// {
+//     int x = 0;
+//     struct node* temp;
+//     temp = head;
+//     while(temp!= nullptr)
+//     {
+//         x++;
+//         temp = temp -> next;
+//     }
+//     return x;
+// }
+
+void displayLL(struct node* head) //To display Singly Linked List.
+{
+   struct node* temp;
+   temp = head;
+   while(temp != 0)
+   {
+        cout<<temp->data;
+        cout<<" ";
+        temp = temp->next;
+   }
+cout<<endl;
+}
+node* delBeg(struct node* head)
+{
+    struct node* temp;
+    temp = head;
+    head = head->next;
+    delete temp;
+    return head;
+}
+int main()
+{
+   node* head = CreatLL(); //Create only 3 elements list.
+//    int length = count(head);
+
+//     cout<<"The count of the LinkedList is "<<length<<endl;
+    head = delBeg(head);
+    displayLL(head);
+    return 0;
+}
